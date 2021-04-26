90c65da07d2a534bfdceb6e6b427a9351b1fbd2e
Alex(Zhirui) Yang <1076830028@qq.com>
2021-04-26 11:39:00 +0800

    Update and fix bug

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ __proj_dir = os.path.dirname(os.path.dirname(__file__))
 DATA_DIR = os.path.join(__proj_dir, 'recommender/data')
 
 RAW_DIR = os.path.join(DATA_DIR, 'raw_data')
-PRE_DIR = os.path.join(DATA_DIR, 'precossed_data')
+PRE_DIR = os.path.join(DATA_DIR, 'processed_data')
 RES_DIR = os.path.join(__proj_dir, 'res_data')
 
 
